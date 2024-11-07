fa2310f22c014c9bacf18d8feefedc7fce34ca89
hamad470 <hamadurrehman62@gmail.com>
2024-11-07 21:10:08 +0500

    files changed againnnn

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
@@ -133,7 +132,3 @@ def predict():
 import os
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-
-
-
